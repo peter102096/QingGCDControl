6eb5be92c557812270e4960deb22578d42e58bd5
PeterLin <peter102096@gmail.com>
2022-09-06 17:38:21 +0800

    First commit

---
+++ GCDControl/GCDControl.h
@@ -0,0 +1,18 @@
+//
+//  GCDControl.h
+//  GCDControl
+//
+//  Created by PeterLin on 2022/8/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GCDControl.
+FOUNDATION_EXPORT double GCDControlVersionNumber;
+
+//! Project version string for GCDControl.
+FOUNDATION_EXPORT const unsigned char GCDControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GCDControl/PublicHeader.h>
+
+
