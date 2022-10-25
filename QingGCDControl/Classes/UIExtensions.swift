3b0a4f6654221323e93fc425ac8dd2eacbd53208
PeterLin <peter102096@gmail.com>
2022-10-25 14:34:33 +0800

    update to 0.0.5

---
+++ QingGCDControl/Classes/UIExtensions.swift
@@ -0,0 +1,38 @@
+//
+//  UIExtensions.swift
+//  QingGCDControl
+//
+//  Created by PeterLin on 2022/9/21.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
