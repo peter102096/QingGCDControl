d7a785e9970c3f605d5f891c5f3a0f046dba8ac0
PeterLin <peter102096@gmail.com>
2022-12-14 14:41:08 +0800

    update to 0.0.14
    Modify API model to new method.

--- QingGCDControl/Classes/API.swift
+++ QingGCDControl/Classes/API.swift
@@ -14,10 +14,10 @@ open class API: NSObject {
     private var trustPort: Int = 443
     private lazy var sharedSession: Session = {
         let manager = ServerTrustManager(evaluators: [trustIP: DisabledTrustEvaluator()])
-           let configuration = URLSessionConfiguration.af.default
-           configuration.timeoutIntervalForRequest = 30
-           return Session(configuration: configuration, serverTrustManager: manager)
-       }()
+        let configuration = URLSessionConfiguration.af.default
+        configuration.timeoutIntervalForRequest = 30
+        return Session(configuration: configuration, serverTrustManager: manager)
+    }()
     var statusCode: Int? = 404
     
     public override init() {
@@ -40,26 +40,24 @@ open class API: NSObject {
     }
     
     public func connectToServer<T: Codable>(url: String, method: HTTPMethod = .get, dataStruct struct: T.Type, params: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default, completion: @escaping (Int?, Codable?) -> Void) {
-
-        sharedSession.request(url, method: method, parameters: params, encoding: encoding).responseJSON { [unowned self] (data) in
-            print("data : \(data)")
+        
+        sharedSession.request(url, method: method, parameters: params, encoding: encoding).responseDecodable(of: T.self) { [unowned self] (data) in
             statusCode = data.response?.statusCode
             print("statusCode : \(statusCode ?? 9999)")
-            guard data.error == nil else {
-                print("error : \(data.error!.localizedDescription)")
-                let errorData = ResultModel(result: data.error!.localizedDescription)
-                completion(statusCode, errorData)
-                return
-            }
-            if let responseData = data.data, statusCode != nil, (statusCode == 200 || statusCode == 201) {
-                guard let result = try? JSONDecoder().decode(T.self, from: responseData) else {
-                    print("\(TAG) decode failed")
+            switch data.result {
+            case .success(_):
+                completion(statusCode, data.value)
+                break
+            case .failure(_):
+                print("\(TAG) connectToServer failure")
+                if data.error == nil {
                     let errorData = ResultModel(result: "Data decode failed")
                     completion(statusCode, errorData)
-                    return
+                } else {
+                    let errorData = ResultModel(result: "\(data.error!.localizedDescription)")
+                    completion(statusCode, errorData)
                 }
-                completion(statusCode, result)
-                return
+                break
             }
         }
     }
